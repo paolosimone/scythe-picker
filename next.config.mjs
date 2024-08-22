ede7d2d4a669ef05a988da1adc50e3576c2879d8
Paolo Simone <p.simone@live.com>
2024-08-22 18:30:33 +0200

    gha

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Static export for GH Pages
   output: "export",
+  basePath: "/scythe-picker",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
